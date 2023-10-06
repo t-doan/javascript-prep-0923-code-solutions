a5fdd295829a94ca1376791496c5452431df4c23
Tai Doan <DoanTai11@gmail.com>
2023-10-06 19:27:41 +0000

    Completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+var addTwoNumbersResult = addTwoNumbers(2, 3);
+console.log('addTwoNumbers result: ', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+var convertHoursToMinutesResult = convertHoursToMinutes(5);
+console.log('convertHoursToMinutes result: ', convertHoursToMinutesResult);
+
+function getGreeting(name) {
+  return 'Hello ' + name;
+}
+var getGreetingResult = getGreeting('Tai');
+console.log('getGreeting result: ', getGreetingResult);
+
+function addAndMultiplyby5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+var addAndMultiplyby5Result = addAndMultiplyby5(2, 4);
+console.log('addAndMultiplyby5 result: ', addAndMultiplyby5Result);
+
+function multipleAndDivideby5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+var multipleAndDivideby5Result = multipleAndDivideby5(10, 2);
+console.log('multipleAndDivideby5 result: ', multipleAndDivideby5Result);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+var subtractTwoNumbersResult = subtractTwoNumbers(10, 7);
+console.log('subtractTwoNumbers result: ', subtractTwoNumbersResult);
+
+function getCircleCircumference(radius) {
+  return 2 * 3.14 * radius;
+}
+var getCircleCircumferenceResult = getCircleCircumference(5);
+console.log('getCircleCircumference result: ', getCircleCircumferenceResult);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+var getFullNameResult = getFullName('Tai', 'Doan');
+console.log('getFullName result: ', getFullNameResult);
+
+function cube(number) {
+  return number * number * number;
+}
+var cubeResult = cube(3);
+console.log('cube result: ', cubeResult);
